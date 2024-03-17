387381c64fbbd655596a391310003e9c94ae10c8
Jihan <jihanahmed41@gmail.com>
2024-03-17 03:15:50 -0700

    task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- create database hbnb_test_db
+-- create a new user hbnb_test
+-- hbnb_test should have all privileges on the database hbnb_test_db (and only this database)
+-- hbnb_test should have SELECT privilege on the database performance_schema (and only this database)
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
