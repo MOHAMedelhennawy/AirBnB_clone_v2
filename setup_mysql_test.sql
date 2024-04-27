b5843c7ad86f1916be606081dd94b8458f7e1006
root <root@DESKTOP-40K4BTE.localdomain>
2024-04-27 20:34:56 +0300

    adding setup mysql files

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Script that creates database hbnb_test and user hbnb_test
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost';
