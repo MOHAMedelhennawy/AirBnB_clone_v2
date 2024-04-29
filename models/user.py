bda9e0aa082384951af53e280feb82bdddbbe7e7
Jihan Ahmed <jihanahmed41@gmail.com>
2024-04-29 12:28:45 -0700

    update user model

--- models/user.py
+++ models/user.py
@@ -22,5 +22,5 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
 
-    places = relationship('Place', backref='user', cascade='all, delete')
-    reviews = relationship('Review', backref='user', cascade='all, delete')
+    places = relationship('Place', backref=backref('user', cascade='all, delete'))
+    reviews = relationship('Review', backref=backref('user', cascade='all, delete'))
