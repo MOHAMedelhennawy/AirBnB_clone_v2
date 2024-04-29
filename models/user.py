c447fe4351c8866720c1cf35eeb9b79876469613
Jihan Ahmed <jihanahmed41@gmail.com>
2024-04-29 12:55:25 -0700

    fix user model:

--- models/user.py
+++ models/user.py
@@ -19,8 +19,8 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
     places = relationship('Place', backref='user', cascade='all, delete')
     reviews = relationship('Review', backref='user', cascade='all, delete')
