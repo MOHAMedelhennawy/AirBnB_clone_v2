1c9ce05e0cf6b5ec62d7af26f2bb11ef27e720ba
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-01 14:12:40 +0300

    update amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -2,11 +2,16 @@
 """
 Amenity class, a subclass of BaseModel
 """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 
-
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column("place_id", String(60), ForeignKey("places.id"), nullable=False, primary_key=True),
+    Column("amenity_id", String(60), ForeignKey("amenities.id"), nullable=False, primary_key=True)
+)
 class Amenity(BaseModel):
     """
     A subclass of BaseModel class
