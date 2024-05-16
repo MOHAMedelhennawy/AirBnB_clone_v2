1b47d82d247c3986521296f355600c6a9b8b46f1
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-16 13:45:52 +0300

    Fix Task-0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,12 +18,10 @@ sudo tee "$FILE" > /dev/null <<EOF
 EOF
 
 SYMLINK="/data/web_static/current"
-[ -e "$SYMLINK" ] && sudo rm -f "$SYMLINK"
 sudo ln -sf /data/web_static/releases/test/ "$SYMLINK"
 sudo chown -hR ubuntu: /data/
 
 NEW_STRING="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 FILE_PATH=/etc/nginx/sites-available/default
-sudo cat -n /etc/nginx/sites-enabled/default | grep -C 5 "}" | grep -C 5 "location /hbnb_static"
 sudo sed -i "s|server_name _;|${NEW_STRING}|g" $FILE_PATH
-service nginx restart
+sudo service nginx restart
