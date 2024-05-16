e28b119ead8f6b46e4455e384d652f108003ea28
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-16 14:24:25 +0300

    Fix Task-0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-#Bash script that sets up your web servers for the deployment of web_static. It must:
+if ! which nginx  > /dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y nginx
+fi
 
-sudo apt-get update
-sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/{releases/test,shared}
 
-# sudo mkdir -p /data/web_static/{releases/test,shared}
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
 FILE="/data/web_static/releases/test/index.html"
 sudo tee "$FILE" > /dev/null <<EOF
 <html>
@@ -19,10 +18,12 @@ sudo tee "$FILE" > /dev/null <<EOF
 EOF
 
 SYMLINK="/data/web_static/current"
+[ -e "$SYMLINK" ] && sudo rm -f "$SYMLINK"
 sudo ln -sf /data/web_static/releases/test/ "$SYMLINK"
-sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -hR ubuntu: /data/
 
 NEW_STRING="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 FILE_PATH=/etc/nginx/sites-available/default
+sudo cat -n /etc/nginx/sites-enabled/default | grep -C 5 "}" | grep -C 5 "location /hbnb_static"
 sudo sed -i "s|server_name _;|${NEW_STRING}|g" $FILE_PATH
-sudo service nginx restart
+service nginx restart
