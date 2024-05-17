ad767f4ee435963b50b355ae2cbd0a1546bea4d6
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-17 10:47:19 +0300

    Fix Task-2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,14 +32,17 @@ def do_deploy(archive_path):
 
     # file_name = archive_path.split("/")[-1].strip(".tgz")
     file_name = path.basename(archive_path).split('.')[0]
-    uncompress_path = "/data/web_static/releases/{}".format(file_name)
+    no_exc = file_name.split('.')[0]
+    uncompress_path = "/data/web_static/releases/{}".format(no_exc)
 
-    tmp_path = '/tmp/'
+    tmp_path = '/tmp/{}'.format(file_name)
     try:
         upload = put(archive_path, tmp_path)
         sudo("mkdir -p {}".format(uncompress_path))
-        sudo("tar -xvzf {}{} -C {}".format(tmp_path, archive_path, uncompress_path))
-        sudo("rm -r {}{}".format(tmp_path, archive_path))
+        sudo("tar -xvzf {} -C {}".format(tmp_path, uncompress_path))
+        sudo("rm {}".format(tmp_path))
+        sudo("mv {0}/web_static/* {0}/".format(uncompress_path))
+        sudo("rm -rf {}/web_static".format(uncompress_path))
         sudo("rm -f /data/web_static/current")
         sudo("ln -s {} /data/web_static/current".format(uncompress_path))
         return True
