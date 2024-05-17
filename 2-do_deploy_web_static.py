eff939efd2f5d3145aad0524273564867ac84e87
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-17 11:18:48 +0300

    Fix Task-2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,23 +7,6 @@ from os import path, makedirs
 env.hosts = ['100.26.210.165', '100.25.48.160']
 env.user = 'ubuntu'
 
-def do_pack():
-    """ Fabric script that generates a .tgz archive
-    from the contents of the web_static folder"""
-
-    date = datetime.now()
-    archive_name = "web_static_{}".format(date.strftime("%Y%m%d%H%M%S"))
-    archive_path = "versions/{}.tgz".format(archive_name)
-
-    if not path.exists("versions"):
-        makedirs("./versions")
-
-    result = local("tar -cvzf {} web_static/".format(archive_path))
-    if result.succeeded:
-        return archive_path
-    else:
-        return None
-
 def do_deploy(archive_path):
     """ Fabric script that distributes an archive to your web servers """
 
