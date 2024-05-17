6673a727053968c4043273747873219bae706945
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-17 07:14:11 +0300

    Add Task-1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,8 +3,10 @@ from fabric.api import local
 from datetime import datetime
 from os import path, makedirs
 
+
 def do_pack():
-    """ Fabric script that generates a .tgz archive from the contents of the web_static folder"""
+    """ Fabric script that generates a .tgz archive
+    from the contents of the web_static folder"""
 
     date = datetime.now()
     archive_name = "web_static_{}".format(date.strftime("%Y%m%d%H%M%S"))
