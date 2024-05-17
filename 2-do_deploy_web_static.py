ae7f766ec347b163cd5ac7580a13eb79de4251a0
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-05-17 11:03:57 +0300

    Fix Task-2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,44 +25,26 @@ def do_pack():
         return None
 
 def do_deploy(archive_path):
-    """Fabric script that distributes an archive to your web servers"""
+    """ Fabric script that distributes an archive to your web servers """
 
-    if not os.path.exists(archive_path):
+    if not path.exists(archive_path):
         return False
 
+    # file_name = archive_path.split("/")[-1].strip(".tgz")
+    file_name = path.basename(archive_path)
+    no_exc = file_name.split('.')[0]
+    uncompress_path = "/data/web_static/releases/{}".format(no_exc)
+
+    tmp_path = '/tmp/{}'.format(file_name)
     try:
-        # Extract the file name and remove the extension
-        file_name = os.path.basename(archive_path)  # e.g., web_static_20170315003959.tgz
-        file_name_no_ext = file_name.split('.')[0]  # e.g., web_static_20170315003959
-
-        # Define the target paths
-        tmp_path = '/tmp/{}'.format(file_name)
-        release_path = "/data/web_static/releases/{}/".format(file_name_no_ext)
-
-        # Upload the archive to the /tmp/ directory on the server
-        put(archive_path, tmp_path)
-
-        # Create the release directory if it doesn't exist
-        sudo("mkdir -p {}".format(release_path))
-
-        # Uncompress the archive to the release directory
-        sudo("tar -xzf {} -C {}".format(tmp_path, release_path))
-
-        # Remove the uploaded archive from the /tmp/ directory
+        upload = put(archive_path, tmp_path)
+        sudo("mkdir -p {}".format(uncompress_path))
+        sudo("tar -xvzf {} -C {}".format(tmp_path, uncompress_path))
         sudo("rm {}".format(tmp_path))
-
-        # Move files out of the web_static directory to the release path
-        sudo("mv {0}/web_static/* {0}/".format(release_path))
-
-        # Remove the now-empty web_static directory
-        sudo("rm -rf {}/web_static".format(release_path))
-
-        # Remove the current symbolic link
+        sudo("mv {0}/web_static/* {0}/".format(uncompress_path))
+        sudo("rm -rf {}/web_static".format(uncompress_path))
         sudo("rm -f /data/web_static/current")
-
-        # Create a new symbolic link to the new release
-        sudo("ln -s {} /data/web_static/current".format(release_path))
-
+        sudo("ln -s {} /data/web_static/current".format(uncompress_path))
         return True
-    except Exception as e:
+    except:
         return False
