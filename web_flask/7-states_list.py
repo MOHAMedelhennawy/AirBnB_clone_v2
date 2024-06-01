0220936594c6fb01b0f23aab0cb5a230e1379dbb
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-01 20:03:28 +0300

    Add

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application."""
-
+""" Starts a Flask web app """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def dispose(exception):
+    """ Remove current session """
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list')
 def states():
-    states = list(storage.all(State).values())
-    return render_template("7-states_list.html", title="States", states=states)
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
