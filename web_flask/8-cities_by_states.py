751dde16db7c51d528f2851996ae3b0be1ee49ce
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-01 22:46:46 +0300

    Add

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Starts a Flask web app """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def dispose(exception):
+    """ Remove current session """
+    storage.close()
+
+
+@app.route('/cities_by_states')
+def states():
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('8-cities_by_states.html', states=states_list)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
