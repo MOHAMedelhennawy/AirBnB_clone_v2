34b09787c6bf135cf2ac60853a9af3bbcf089287
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-03 00:14:16 +0300

    Fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """ Starts a Flask web app """
 from flask import Flask, render_template
@@ -14,24 +15,22 @@ def dispose(exception):
     storage.close()
 
 
+@app.route('/states')
 def states():
     """ Display list of all the states """
     states = storage.all(State)
-    states_list = list(states.values())
-    return render_template('9-states.html', states=states_list, id=None)
+    return render_template('9-states.html', state=states)
 
 
-@app.route('/states')
 @app.route('/states/<id>')
 def statesWithID(id):
     """ Display list of all the states """
-    states = storage.all(State)
-    states_list = list(states.values())
-    for state in states_list:
-        if isinstance(state, State) and id == state.id:
-            cities = state.cities
-    return render_template('9-states.html', state=state, cities=cities, id=id)
-
+    
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', state=state)
+    return render_template('9-states.html')
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
